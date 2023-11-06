18b34671cba6e5bbf13a381b20f4889b04d28ced
Vit <148717142+ivanaMistrov@users.noreply.github.com>
2023-11-06 23:03:49 +0300

    Update victory.py

--- victory.py
+++ victory.py
@@ -1,56 +1,38 @@
-import random
+total = 0
+again = 1
 
-# Создаем словарь с известными людьми и их годами рождения
-celebrities = {
-    "Альберт Эйнштейн": 1879,
-    "Исаак Ньютон": 1643,
-    "Мария Кюри": 1867,
-    "Наполеон Бонапарт": 1769,
-    "Уильям Шекспир": 1564
-}
+enshtein = 1879
+nuton = 1643
+kuri = 1867
+bonapart = 1769
+Schekspir = 1564
 
-def play_quiz():
-    correct_answers = 0
-    wrong_answers = 0
+while again:
+    total = 0
+    a = input("Напиши год рождения Энштейна ")
+    if (enshtein == int(a)):
+        total += 1
 
-    # Перемешиваем известных людей для каждой новой игры
-    list_of_celebrities = list(celebrities.items())
-    random.shuffle(list_of_celebrities)
+    b = input("год рождения Ньютона напиши")
+    if (nuton == int(b)):
+        total += 1
 
-    for name, birth_year in list_of_celebrities:
-        guess = input(f"В каком году родился {name}? ")
-        try:
-            guess = int(guess)
-            if guess == birth_year:
-                print("Правильно!")
-                correct_answers += 1
-            else:
-                print(f"Неправильно. Правильный ответ: {birth_year}")
-                wrong_answers += 1
-        except ValueError:
-            print("Вы ввели некорректный год. Попробуйте снова.")
-            wrong_answers += 1
+    c = input("Год рождения Кюри напиши")
+    if (kuri == int(c)):
+        total += 1
 
-    total_questions = correct_answers + wrong_answers
-    correct_percentage = (correct_answers / total_questions) * 100
-    wrong_percentage = (wrong_answers / total_questions) * 100
+    d = input("Год рождения Наполеона напиши")
+    if (bonapart == int(d)):
+        total += 1
 
-    print(f"\nКоличество правильных ответов: {correct_answers}")
-    print(f"Количество ошибок: {wrong_answers}")
-    print(f"Процент правильных ответов: {correct_percentage}%")
-    print(f"Процент неправильных ответов: {wrong_percentage}%")
+    f = input("Год рождения Шекспира напиши")
+    if (Schekspir == int(f)):
+        total += 1
 
-# Запускаем игру
-play_quiz()
-
-# Проверяем, хочет ли пользователь начать игру заново
-while True:
-    play_again = input("Хотите начать игру заново? (да/нет): ")
-    if play_again.lower() == "да":
-        play_quiz()
-    elif play_again.lower() == "нет":
-        print("Спасибо за игру! До свидания!")
-        break
+    print("ты " + str(total) + " ответов отгадал")
+    print("это  целых % " + str(total * 100 / 5) + " ответов ")
+    again = input("еще разок? 1 = да 2 = нет")
+    if (int(again) == 1):
+        continue
     else:
-        print("Пожалуйста, введите 'да' или 'нет'.")
-
+        break
