e317ad6fcb72be71095f3f619b59cf946febeb80
Vit <148717142+ivanaMistrov@users.noreply.github.com>
2023-11-08 21:17:31 +0300

    Update bornyear.py

--- bornyear.py
+++ bornyear.py
@@ -1,11 +1,11 @@
-'''yearPushkin = 1744
+'''yearPushkin = 1799
 year = int(input('год рождения Пушкина :'))
 while year != yearPushkin:
     print("Неверно")
     year = int(input('год рождения Пушкина :'))
 print('вы угадали ')'''
 
-yearPushkin = 1744
+yearPushkin = 1799
 year = int(input('год рождения Пушкина :'))
 if yearPushkin == year:
     print('верно')
